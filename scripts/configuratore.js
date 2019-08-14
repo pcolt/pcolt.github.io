aef317d40eb3bcc91a6efad11c27bb5f79607b37
pcolt <pablo.colturi@gmail.com>
2019-08-14 10:13:30 +0100

    Update configuratore.js

--- scripts/configuratore.js
+++ scripts/configuratore.js
@@ -2,27 +2,11 @@ var inputLatoCorto = document.querySelector('.inputLatoCorto');
 var inputLatoLungo = document.querySelector('.inputLatoLungo');
 var pNumPannelli = document.querySelector('.pNumPanelli');
 
-function squared(num) {
-  return num * num;
-}
 
-function cubed(num) {
-  return num * num * num;
-}
-
-function factorial(num) {
-  var x = num;
-  while (x > 1) {
-    num *= x-1;
-    x--;
-  }
-  return num;
-}
 
 inputLatoCorto.onchange = function() {
   var latoCorto = inputLatoCorto.value;
   }
 
-alert(latoCorto);
 
 }
