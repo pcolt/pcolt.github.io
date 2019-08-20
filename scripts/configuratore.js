1b4c16290b2b6fb065656e8636756e17ecb1227a
pcolt <pablo.colturi@gmail.com>
2019-08-20 16:28:54 +0200

    Update configuratore.js

--- scripts/configuratore.js
+++ scripts/configuratore.js
@@ -2,6 +2,9 @@ var inputLatoCorto = document.querySelector('.inputLatoCorto');
 var inputLatoLungo = document.querySelector('.inputLatoLungo');
 var pArea = document.querySelector('p');
 var button = document.querySelector('button');
+var canvas = document.querySelector('.myCanvas');
+
+var ctx = canvas.getContext('2d');
 
 button.onclick = function() {
   var latoCorto = inputLatoCorto.value;
@@ -9,6 +12,9 @@ button.onclick = function() {
   var area = latoCorto * latoLungo;
   pArea.textContent = 'Superficie del tetto: ' + area + ' metri quadrati';
 }
+
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillRect = (0,0,300,150);
 
 
 
