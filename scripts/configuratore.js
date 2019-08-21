a8d23b0385763449520e413d6b5289b4c51d4170
pcolt <pablo.colturi@gmail.com>
2019-08-21 10:12:40 +0200

    Update configuratore.js

--- scripts/configuratore.js
+++ scripts/configuratore.js
@@ -2,12 +2,13 @@ var inputLatoCorto = document.querySelector('.inputLatoCorto');
 var inputLatoLungo = document.querySelector('.inputLatoLungo');
 var pArea = document.querySelector('p');
 var button = document.querySelector('button');
-var canvas = document.querySelector('.myCanvas');
 
-var ctx = canvas.getContext('2d');
-
-ctx.fillStyle = 'rgb(0,0,255)';
-ctx.fillRect = (0,0,300,150);
+function draw() {
+  var canvas = document.querySelector('.myCanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgb(0,0,255)';
+  ctx.fillRect = (0,0,300,150);
+}
 
 button.onclick = function() {
   var latoCorto = inputLatoCorto.value;
