5432e50063f74a8a1a8e638efb8f2c5b914a4ec8
pcolt <pablo.colturi@gmail.com>
2019-08-21 10:31:27 +0200

    Update configuratore.js

--- scripts/configuratore.js
+++ scripts/configuratore.js
@@ -8,6 +8,11 @@ function draw() {
   var ctx = canvas.getContext('2d');
   ctx.fillStyle = 'rgb(0,0,255)';
   ctx.fillRect = (0,0,300,150);
+  
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 50, 50);
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
 }
 
 button.onclick = function() {
