93a428e4b63cce08cac6070b734fdbedcbfb7e0d
pcolt <pablo.colturi@gmail.com>
2019-10-25 16:57:30 +0200

    Update configuratore.js

--- scripts/configuratore.js
+++ scripts/configuratore.js
@@ -15,58 +15,39 @@ button.onclick = function() {
   var orientamento = inputOrientam.value;
   //calcolo area falda
   var area = latoOriz * latoVert;
-  
-  //disego nel canvas
-  var canvas = document.querySelector('.myCanvas');
-  var ctx = canvas.getContext('2d');
-  //ripulisce eventuale precedente disegno con un quadrato bianco
-  ctx.fillStyle = 'rgb(255,255,255)';
-  ctx.fillRect(0,0,600,600);
-  var scala = 20;
-  //un rettangolo con le dimensioni del tetto moltiplicate per la scala
-  ctx.fillStyle = 'rgb(240,240,240)';
-  ctx.fillRect(0,0,latoOriz*scala,latoVert*scala);
-  ctx.strokeStyle = 'rgb(0,0,0)';
-  ctx.strokeRect(0,0,latoOriz*scala,latoVert*scala);
-  //posizione del primo modulo MAS, 6 = 0,3 m x 20
-  var x = 6;
-  var y = 6;
+  //inizializzo variabile lunghezza totale coperta dai moduli
+  var ltot = 0;
+  var xtot = 0;
+  var ytot = 0;
+  //inizializzo variabile per conteggio numero moduli
   var count = 0;
-  //doppio loop per disegnare tutti i moduli con dimensioni scalate di 20 e margine di (0,3m x 20 = 6)
-  //primo loop se disposizione dei pannelli selezionata in 'verticale'
-  if (orientamento === 'Verticale') {
-  while (y <= (latoVert*scala - 1.82*scala -6)) {
-    while (x <= (latoOriz*scala - 1*scala - 6)) {
-      ctx.fillStyle = 'rgb(0,0,200)';
-      ctx.fillRect(x,y,1*scala,1.82*scala);
-      ctx.strokeStyle = 'rgb(0, 0, 0)';
-    	//ctx.lineWidth = 5;
-    	ctx.strokeRect(x,y,1*scala,1.82*scala);
-    	x += 1*scala;
+
+  if (orientamento === 'Orizzontale') {
+    while (ltot <= latoOriz) {
+      let i = 0; 
+      xtot += 1.82 * Math.cos(i);
+      ytot += 1.82 * Math.sin(i);
+      i += 2;
       count += 1;
-    	}
-   x = 6;
-   y += 1.82*scala;
-   }
-  //secondo loop se disposizione dei pannelli selezionata in 'orizzontale'
+      ltot = Math.sqrt((xtot * xtot) + (ytot * ytot))
+      }
+    count += latoVert / 1;
+  }
+  //secondo loop se disposizione dei pannelli selezionata in 'Verticale'
   } else {
-    while (y <= (latoVert*scala - 1*scala -6)) {
-      while (x <= (latoOriz*scala - 1.82*scala - 6)) {
-        ctx.fillStyle = 'rgb(0,0,200)';
-        ctx.fillRect(x,y,1.82*scala,1*scala);
-        ctx.strokeStyle = 'rgb(0, 0, 0)';
-    	  //ctx.lineWidth = 5;
-    	  ctx.strokeRect(x,y,1.82*scala,1*scala);
-    	  x += 1.82*scala;
-        count += 1;
-    	 }
-    x = 6;
-    y += 1*scala;
-   }
+    while (ltot <= latoOriz) {
+      let i = 0; 
+      xtot += 1 * Math.cos(i);
+      ytot += 1 * Math.sin(i);
+      i += 2;
+      count += 1;
+      ltot = Math.sqrt((xtot * xtot) + (ytot * ytot))
+      }
+    count += latoVert / 1.82;
   }
   
   //scrivo la superficie, numero pannelli, superficie captante...
-  pResult.textContent = 'Superficie falda: ' + area + ' metri quadrati \r\n' + 
+  pResult.textContent = 'Superficie da coprire: ' + area + ' metri quadrati \r\n' + 
     'Numero totale pannelli: ' + count + '\r\n' + 
     'Superficie totale captante: ' + count*1.78 + ' metri quadrati \r\n' +
     'Potenza nominale: ' + count*300 + ' Wp';
